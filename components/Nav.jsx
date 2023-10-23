9c85b97329fa78a98c1daf4bd474a1d825fb80de
arifnayan2861 <am.nayan2861@gmail.com>
2023-10-23 17:13:35 +0600

    Authentication & DB & others

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,6 +8,7 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 const Nav = () => {
   const isUserLoggedIn = true;
   const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
     const setProviders = async () => {
@@ -64,7 +65,7 @@ const Nav = () => {
         )}
       </div>
       {/* Mobile Navigation */}
-      <div className="flex-relative sm:hidden">
+      <div className="flex relative sm:hidden">
         {isUserLoggedIn ? (
           <div className="flex">
             <Image
@@ -73,8 +74,36 @@ const Nav = () => {
               width={37}
               height={37}
               alt="profile"
-              onClick={() => {}}
+              onClick={() => setToggleDropdown((prev) => !prev)}
             />
+            {toggleDropdown && (
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type="button"
+                  className="mt-5 w-full black_btn"
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
           </div>
         ) : (
           <>
--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,7 +1,9 @@
+"use client";
 import React from "react";
+import { SessionProvider } from "next-auth/react";
 
-const Provider = () => {
-  return <div>Provider</div>;
+const Provider = ({ session, children }) => {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 };
 
 export default Provider;
